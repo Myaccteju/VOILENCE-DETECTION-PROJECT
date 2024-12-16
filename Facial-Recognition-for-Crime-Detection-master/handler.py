716a89eb15c5c7c1bc7c86eb5e6d0491e79073e9
Tejashree Mestry <112766428+Myaccteju@users.noreply.github.com>
2024-12-16 13:08:56 +0530

    Delete Facial-Recognition-for-Crime-Detection-master directory

--- Facial-Recognition-for-Crime-Detection-master/handler.py
+++
@@ -1,9 +0,0 @@
-import csv
-def insertData(data):
-    field=['Name', "Father's Name", "Gender", "DOB","Crimes"]
-    x=[data['Name'], data["Father's Name"], data['Gender'], data['DOB(yyyy-mm-dd)'], data['Crimes Done']]
-    filen = "Criminal.csv"
-    with open(filen, 'w') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(field)
-        csvwriter.writerow(x)
